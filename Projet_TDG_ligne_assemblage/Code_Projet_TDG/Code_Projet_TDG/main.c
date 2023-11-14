c7fa21062341b017f16f16d16c78d9dd26313e5b
OrpheLafarge <131762127+OrpheLafarge@users.noreply.github.com>
2023-11-14 11:02:43 +0100

    Update main.c struc exclusion

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Structure pour représenter une paire d'opérations interdites
+typedef struct {
+    int op1;
+    int op2;
+} Exclusion;
+
 int main()
 {
     printf("Hello world!\n");
