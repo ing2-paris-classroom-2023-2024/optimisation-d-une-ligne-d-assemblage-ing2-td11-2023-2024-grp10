cc820c4c9070fc908a07a6ddfaa6613069e368f8
lolaskypiea <122100464+lolaskypiea@users.noreply.github.com>
2023-11-26 21:27:02 +0100

    commentaires

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/tempscycle.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/tempscycle.c
@@ -1,19 +1,24 @@
 //
 // Created by lolas on 26/11/2023.
 //
-//tzsr Test ,
+//
 #include "tempscycle.h"
 
+
+// sous programme pour lire dans le ficher le temps de cycle
 float liretempscycle(const char* nomfichier) {
     f* fichier = fopen(nomfichier, "r");
+    // on declare un pointeur pour ouvrir le fichier
     if (fichier == NULL) {
         printf("Erreur d'ouverture du fichier TEMPS CYCLES ");
         exit(1);
     }
+    // blindage
 
 
     float tempscycle;
     if (fscanf(fichier, "%f", &tempscycle) != 1) {
+        // on lis le fichier
         printf("Erreur lecture du TEMPS DE CYCLE");
         exit(1);
     }
