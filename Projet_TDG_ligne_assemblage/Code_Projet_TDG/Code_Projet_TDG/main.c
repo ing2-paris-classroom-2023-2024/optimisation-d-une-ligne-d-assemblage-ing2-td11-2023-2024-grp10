aee8a9a9824cdd27e1659d68074f7197b0efe6d3
GaelCalligaro <132274505+GaelCalligaro@users.noreply.github.com>
2023-11-27 11:24:55 +0100

    Update main.c
    modif

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure pour représenter une paire d'opérations interdites ce q
+// Structure pour représenter une paire d'opérations interdites ce qu
 typedef struct {   
     int op1;
     int op2;
