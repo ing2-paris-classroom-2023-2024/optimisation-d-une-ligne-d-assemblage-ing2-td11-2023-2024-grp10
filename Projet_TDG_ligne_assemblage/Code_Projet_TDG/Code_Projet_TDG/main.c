73181b5f6ea15a474dbeaf43eab16eaa6f19d8ff
GaelCalligaro <132274505+GaelCalligaro@users.noreply.github.com>
2023-11-27 11:16:20 +0100

    Update main.c
    pu d'espace

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure pour représenter une paire d'opérations interdites 
+// Structure pour représenter une paire d'opérations interdites
 typedef struct {   
     int op1;
     int op2;
