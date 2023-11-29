8743d164da211380f6461640d43c2b8ec7e6a645
GaelCalligaro <132274505+GaelCalligaro@users.noreply.github.com>
2023-11-29 10:26:20 +0100

    Update main.c
    modif

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
@@ -7,7 +7,7 @@ typedef struct {
     int op2;
 } Exclusion;
 
-// Structure qui permet de représenter le graphe orienté sans cycle (acyclique)
+// CStructure qui permet de représenter le graphe orienté sans cycle (acyclique)
 typedef struct {
     int numVertices; // Nombre total d'opérations
     int numExclusions; // Nombre de paires d'opérations interdites
