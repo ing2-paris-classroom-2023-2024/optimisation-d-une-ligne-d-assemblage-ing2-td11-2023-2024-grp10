61bac2fb3f5666290bb148dc67ffa3da53e46769
Orphé Lafarge <131762127+OrpheLafarge@users.noreply.github.com>
2023-12-04 00:40:01 +0100

    Update precedences.c
    struct graphe

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/precedences.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/precedences.c
@@ -14,3 +14,11 @@ typedef struct Node {
     int operation;
     struct Node* next;
 } Node;
+
+// Structure pour représenter le graphe de précédence
+typedef struct Graph {
+    int numOperations;
+    Node* adjList[MAX_OPERATIONS];
+    int inDegree[MAX_OPERATIONS];
+    int isSpecified[MAX_OPERATIONS];
+} Graph;
