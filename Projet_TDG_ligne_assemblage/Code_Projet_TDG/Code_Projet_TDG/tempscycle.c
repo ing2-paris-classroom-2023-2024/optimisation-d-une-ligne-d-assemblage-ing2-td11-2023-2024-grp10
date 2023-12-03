7e5ea0c7730264d43b75a96485efc3db7ed76769
lolaskypiea <122100464+lolaskypiea@users.noreply.github.com>
2023-12-03 14:05:59 +0100

    TEMPS CYCLES V2

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/tempscycle.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/tempscycle.c
@@ -3,11 +3,13 @@
 //
 //
 #include "tempscycle.h"
-/*
 
-// sous programme pour lire dans le ficher le temps de cycle
+
+
+// Structure qui permet de représenter une opération avec son temps d'exécution
+
 float liretempscycle(const char* nomfichier) {
-    f* fichier = fopen(nomfichier, "r");
+    FILE * fichier = fopen(nomfichier, "r");
     // on declare un pointeur pour ouvrir le fichier
     if (fichier == NULL) {
         printf("Erreur d'ouverture du fichier TEMPS CYCLES ");
@@ -25,4 +27,70 @@ float liretempscycle(const char* nomfichier) {
 
     fclose(fichier);
     return tempscycle;
+}
+int lireoperations(const char *fileName, Operation *operations, int maxSize) {
+    FILE *file;
+    int count = 0;
+
+    file = fopen(fileName, "r");
+    if (file == NULL) {
+        perror("Erreur en ouvrant le fichier");
+        return -1;
+    }
+
+    while (fscanf(file, "%d %lf", &operations[count].operationNumber, &operations[count].executionTime) == 2) {
+        count++;
+        if (count >= maxSize) {
+            printf("Limite de capacité du tableau atteinte.\n");
+            break;
+        }
+    }
+
+    fclose(file);
+    return count;
+}
+/*
+// pour verifier le graph avec l'affichage
+void printOperations(const Operation *operations, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("Operation %d: Temps d'execution = %.2f\n", operations[i].operationNumber, operations[i].executionTime);
+    }
 }*/
+void creerStations(Operation *operations, int size, float tempsCycle) {
+    // on initialise la première station a 1
+    int stationNumber = 1;
+    float tempsTotal = 0.0;
+    float tempsTotalStation = 0.0;
+
+    printf("Station %d:\n", stationNumber);
+    for (int i = 0; i < size; i++) {
+        if (tempsTotal + operations[i].executionTime > tempsCycle) {
+            printf("    Temps total de la Station %d: %.2f\n", stationNumber, tempsTotalStation);
+            stationNumber++;
+            tempsTotal = 0.0f;
+            tempsTotalStation = 0.0f;
+            printf("\nStation %d:\n", stationNumber);
+        }
+        printf("    Operation %d: Temps d'execution = %.2f\n", operations[i].operationNumber, operations[i].executionTime);
+        tempsTotal += operations[i].executionTime;
+        tempsTotalStation += operations[i].executionTime;
+    }
+    printf("    Temps total de la Station %d: %.2f\n", stationNumber, tempsTotalStation);
+}
+
+
+int main() {
+    // on init notre tab de structures
+    Operation operations[100];
+    // on recup dans les fichiers le temps de cycles et les operations avec leur temps d'execution
+    float tempsCycle = liretempscycle("../temps_cycle.txt");
+    int size = lireoperations("../operations.txt", operations, 100);
+
+
+    if (size > 0) {
+        // on applique le code pour creer les station
+        creerStations(operations, size, tempsCycle);
+    }
+
+    return 0;
+}
--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/tempscycle.h
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/tempscycle.h
@@ -7,5 +7,12 @@
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD11_2023_2024_GRP10_TEMPSCYCLE_H
 #include <stdio.h>
 #include <stdlib.h>
-//float liretempscycle(const char* nomfichier);
+typedef struct {
+    int operationNumber;
+    double executionTime;
+} Operation;
+
+float liretempscycle(const char* nomfichier);
+int lireoperations(const char *fileName, Operation *operations, int maxSize);
+void creerStations(Operation *operations, int size, float tempsCycle);
 #endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TD11_2023_2024_GRP10_TEMPSCYCLE_H
