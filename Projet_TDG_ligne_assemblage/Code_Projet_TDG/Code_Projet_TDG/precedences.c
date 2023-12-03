5e29cc51ef8e2c93f3573c3031cd0614a1c43cbc
Orphé Lafarge <131762127+OrpheLafarge@users.noreply.github.com>
2023-12-04 00:39:00 +0100

    Create precedences.c

---
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/precedences.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_OPERATIONS 100
+
+// Structure pour représenter une opération avec son temps d'exécution
+typedef struct {
+    int operationNumber;
+    double executionTime;
+} Operation;
