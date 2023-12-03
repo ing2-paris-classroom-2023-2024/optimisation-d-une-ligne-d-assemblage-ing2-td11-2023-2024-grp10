df0a642286b20132d2c8b0810b83318972a975f2
victorbgr <132351449+victorbgr@users.noreply.github.com>
2023-12-03 19:51:17 +0100

    Update main.c
    modif

--- Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
+++ Projet_TDG_ligne_assemblage/Code_Projet_TDG/Code_Projet_TDG/main.c
@@ -21,7 +21,7 @@ typedef struct {
     double executionTime;
 } Operation;
 
-// Fonction pour initialiser le graphe
+// Fonction pour initialiser le 
 void initializeGraph(Graph *graph, int numVertices) {
     graph->numVertices = numVertices;
     graph->numExclusions = 0;
